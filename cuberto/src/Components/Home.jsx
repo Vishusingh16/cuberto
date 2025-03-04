66c824b550df7c249343c8ddbab5add8f6ca6635
Vaishnav Singh <vaishnavsingh162@gmail.com>
2025-03-04 20:08:04 +0530

    git commit

--- cuberto/src/Components/Home.jsx
+++ cuberto/src/Components/Home.jsx
@@ -1,31 +1,72 @@
-import React from 'react'
+import React from "react";
+import { useGSAP } from '@gsap/react';
+import gsap from "gsap";
 
 const Home = () => {
-  return (
-    <div className='h-[86vh] w-full bg-zinc-300 px-40 pt-16 bg-white'>
-      <div>
-        <h1 className='text-9xl font-normal tracking-tighter'>
-            <span>We </span> digital 
-            <span>are  </span>
-            <span>a </span>
-            <span>digital </span>
-        </h1>
-      </div>
-      <div className='flex items-center gap-4'>
-        <video className='object-contain h-32 rounded-[5vw]'src="https://cuberto.com/assets/home/hero/header.mp4" autoPlay loop muted></video>
-        <h1 className='text-9xl font-normal tracking-tighter'>
-          <span className='italic font-light'>
-            design
-            </span> and
-        </h1>
-      </div>
-      <div>
-        <h1 className='text-9xl font-normal tracking-tighter'>
-          motion agency
-        </h1>
-      </div>
-    </div>
-  )
-}
+    useGSAP(() => {
+     
+        gsap.from(".headText span", {
+            y: 200, 
+            opacity: 0, 
+            duration: 2, 
+            ease: "expo", 
+            stagger: 0.08, 
+            yoyo: false,
+        });
 
-export default Home
+        const video = document.querySelector("video");
+        if (video) {
+            video.addEventListener("play", () => {
+                gsap.to(".headText span", {
+                    y: 0, 
+                    opacity: 1, 
+                    duration: 1,
+                    ease: "power3.in",
+                    stagger: 0.08,
+                });
+            });
+        }
+    }, []);
+
+    return (
+        <div className='h-screen w-full px-[10%] pt-[5vh] bg-white overflow-hidden'>
+          
+            <div className='max-w-[1600px] mx-auto'>
+           
+                <div className='relative'>
+                    <h1 className='text-[7.8vw] font-[350] leading-[1.4] headText'>
+                        <span className='inline-block pr-[0.25em]'>We</span>
+                        <span className='inline-block pr-[0.25em]'>are</span>
+                        <span className='inline-block pr-[0.25em]'>a</span>
+                        <span className='inline-block'>digital</span>
+                    </h1>
+                </div>
+
+              
+                <div className='flex items-baseline gap-[2vw] mt-[-1.5vw]'>
+                    <video 
+                        className='h-[8.5vw] rounded-[2vw] object-cover flex-shrink-0'
+                        src="https://cuberto.com/assets/home/hero/header.mp4" 
+                        autoPlay 
+                        loop 
+                        muted
+                        playsInline 
+                    />
+                    <h1 className='text-[7.8vw] font-[350] leading-[1.4] headText'>
+                        <span className='italic font-[300] inline-block pr-[0.25em]'>design</span>
+                        <span className='inline-block'>and</span>
+                    </h1>
+                </div>
+
+                <div className='mt-[-1vw]'>
+                    <h1 className='text-[7.8vw] font-[350] leading-[0.9] headText'>
+                        <span className='inline-block pr-[0.25em]'>motion</span>
+                        <span className='inline-block'>agency</span>
+                    </h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
